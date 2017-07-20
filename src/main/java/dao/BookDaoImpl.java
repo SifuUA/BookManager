c3199479d8a8b70f0534610b1379ce57adf4a185
SifuUA <alekseykres@gmail.com>
2017-07-20 14:20:14 +0300

    added class BookDaoImpl, class must be @Repository, work with sessionfactory and session

---
+++ src/main/java/dao/BookDaoImpl.java
@@ -0,0 +1,76 @@
+package dao;
+
+import model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by Alex on 20.07.2017.
+ */
+@Repository
+public class BookDaoImpl implements BookDao {
+
+    private static final Logger logger = LoggerFactory.getLogger(BookDaoImpl.class);
+
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void addBook(Book book) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(book);
+        logger.info("Book successfully added " + book);
+    }
+
+    @Override
+    public void updateBook(Book book) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.update(book);
+        logger.info("Book successfully updated " + book);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Book> listBooks() {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<Book> bookList = session.createQuery("from Books").list();
+
+        for (Book book : bookList) {
+            logger.info("Book lsit " + book);
+        }
+        return bookList;
+    }
+
+    @Override
+    public Book getBookByID(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Book book = (Book) session.load(Book.class, new Integer(id));
+        logger.info("Book successfully loaded " + book);
+        return book;
+    }
+
+    @Override
+    public void removeBook(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Book book = (Book) session.load(Book.class, new Integer(id));
+        if (book != null){
+            session.delete(book);
+        }
+
+        logger.info("Book successfully removed " + book);
+    }
+
+    @Override
+    public Collection<Book> getBooks(String search) {
+        return null;
+    }
+}
