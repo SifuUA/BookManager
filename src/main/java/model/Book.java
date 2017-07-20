d7827e46e91c31ded88384f3349423f31f38d309
SifuUA <alekseykres@gmail.com>
2017-07-20 13:27:13 +0300

    added package model and class Book were implement db anotattions

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,71 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by Alex on 20.07.2017.
+ */
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private int price;
+
+    public Book() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
